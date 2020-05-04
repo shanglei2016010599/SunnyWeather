aa8d624e762ea306ef9c024545cb40c9eda72583
shanglei2016010599 <1014952588@qq.com>
2020-05-04 11:33:25 +0800

    实现搜索全球城市数据功能

---
+++ app/src/main/java/com/sunnyweather/android/SunnyWeatherApplication.kt
@@ -0,0 +1,17 @@
+package com.sunnyweather.android
+
+import android.app.Application
+import android.content.Context
+
+class SunnyWeatherApplication : Application() {
+
+    companion object {
+        const val TOKEN = "j3x1kqUebkGEAev5"
+        lateinit var context: Context
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        context = applicationContext
+    }
+}
---
+++ app/src/main/java/com/sunnyweather/android/logic/network/SunnyWeatherNetWork.kt
@@ -0,0 +1,46 @@
+package com.sunnyweather.android.logic.network
+
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.await
+import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
+import kotlin.coroutines.suspendCoroutine
+
+
+object SunnyWeatherNetWork {
+
+    private val placeService = ServiceCreator.create(PlaceService::class.java)
+
+    suspend fun searchPlaces(query: String) = placeService.searchPlaces(query).await()
+
+    private suspend fun <T> Call<T>.await(): T {
+        return suspendCoroutine { continuation ->
+            enqueue(object : Callback<T> {
+                /**
+                 * Invoked when a network exception occurred talking to the server or when an unexpected
+                 * exception occurred creating the request or processing the response.
+                 */
+                override fun onFailure(call: Call<T>, t: Throwable) {
+                    continuation.resumeWithException(t)
+                }
+
+                /**
+                 * Invoked for a received HTTP response.
+                 *
+                 *
+                 * Note: An HTTP response may still indicate an application-level failure such as a 404 or 500.
+                 * Call [Response.isSuccessful] to determine if the response indicates success.
+                 */
+                override fun onResponse(call: Call<T>, response: Response<T>) {
+                    val body = response.body()
+                    if (body != null) continuation.resume(body)
+                    else continuation.resumeWithException(
+                        RuntimeException("response body is null")
+                    )
+                }
+            })
+        }
+    }
+}
